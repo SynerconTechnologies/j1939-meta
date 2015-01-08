3365b786f03bfda9f5fa21aa5f880c75bda7524f
Patrick Forringer <patrick@forringer.com>
2015-01-08 14:34:42 -0600

    first commit of j1939 meta package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from j1939 import VERSION
+
+
+setup(
+    name="j1939-meta",
+    version=VERSION,
+    author="Patrick Forringer",
+    author_email="patrick@forringer.com",
+    url="https://github.com/SynerconTechnologies/j1939-meta",
+    description="",
+    packages=['j1939'],
+    use_2to3=True)
